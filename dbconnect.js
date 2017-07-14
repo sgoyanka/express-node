d799efd332ca93cbe12accf10b037f22f35bd3c7
sgoyanka <sgoyanka@grepruby.com>
2017-07-14 19:25:51 +0530

    Data succefully added using mongoose schema

--- dbconnect.js
+++ dbconnect.js
@@ -55,37 +55,12 @@ var userSchema = new Schema({
 });
 
 
-userSchema.methods.dudify = function() {
-  // add some stuff to the users name
-  this.name = this.name + '-dude'; 
 
-  return this.name;
-};
 // the schema is useless so far
 // we need to create a model using it
 var User = mongoose.model('User', userSchema);
 
 // make this available to our users in our Node applications
-//module.exports = User;
+module.exports = User;
 
 // create a new user called chris
-var chris = new User({
-  name: 'Chris',
-  username: 'sevilayha',
-  password: 'password' 
-});
-
-// call the custom method. this will just add -dude to his name
-// user will now be Chris-dude
-chris.dudify(function(err, name) {
-  if (err) throw err;
-
-  console.log('Your new name is ' + name);
-});
-
-// call the built-in save method to save to the database
-chris.save(function(err) {
-  if (err) throw err;
-
-  console.log('User saved successfully!');
-});
--- testmongoose.js
+++ testmongoose.js
@@ -1,19 +1,11 @@
-var User = require('./app/models/user');
+var User = require('./dbconnect.js');
 
-// create a new user called chris
 var chris = new User({
   name: 'Chris',
-  username: 'sevilayha',
+  username: 'sevilayha2',
   password: 'password' 
 });
 
-// call the custom method. this will just add -dude to his name
-// user will now be Chris-dude
-chris.dudify(function(err, name) {
-  if (err) throw err;
-
-  console.log('Your new name is ' + name);
-});
 
 // call the built-in save method to save to the database
 chris.save(function(err) {
