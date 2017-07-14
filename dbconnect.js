7bf0c9759d5fad741ac15188023f75555b678ce1
sgoyanka <sgoyanka@grepruby.com>
2017-07-14 18:46:53 +0530

    Created schema using mongoose

--- dbconnect.js
+++ dbconnect.js
@@ -10,17 +10,17 @@ MongoClient.connect('mongodb://127.0.0.1:27017/'+string1, function (err, db) {
         console.log("successfully connected to the database");
         var collection = db.collection('seconddb');
 
-        collection.find().toArray(function(err, results) {
-        console.dir(results);
+        Cursor cursor = collection.find(); 
+        var obj = JSON.parse(cursor.index);
+        console.log(obj);
         // Let's close the db
         db.close();
-    });
     	
     }
 
     
 });*/
-var MongoClient = require('mongodb').MongoClient
+/*var MongoClient = require('mongodb').MongoClient
     , format = require('util').format;
 
 obj = {a:1,b:2,c:3};
@@ -34,4 +34,58 @@ MongoClient.connect('mongodb://127.0.0.1:27017/seconddb', function(err, db) {
             db.close();
         });
     });
+});*/
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/expressdb');
+var Schema = mongoose.Schema;
+
+// create a schema
+var userSchema = new Schema({
+  name: String,
+  username: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  admin: Boolean,
+  location: String,
+  meta: {
+    age: Number,
+    website: String
+  },
+  created_at: Date,
+  updated_at: Date
+});
+
+
+userSchema.methods.dudify = function() {
+  // add some stuff to the users name
+  this.name = this.name + '-dude'; 
+
+  return this.name;
+};
+// the schema is useless so far
+// we need to create a model using it
+var User = mongoose.model('User', userSchema);
+
+// make this available to our users in our Node applications
+//module.exports = User;
+
+// create a new user called chris
+var chris = new User({
+  name: 'Chris',
+  username: 'sevilayha',
+  password: 'password' 
+});
+
+// call the custom method. this will just add -dude to his name
+// user will now be Chris-dude
+chris.dudify(function(err, name) {
+  if (err) throw err;
+
+  console.log('Your new name is ' + name);
+});
+
+// call the built-in save method to save to the database
+chris.save(function(err) {
+  if (err) throw err;
+
+  console.log('User saved successfully!');
 });
---
+++ testmongoose.js
@@ -0,0 +1,23 @@
+var User = require('./app/models/user');
+
+// create a new user called chris
+var chris = new User({
+  name: 'Chris',
+  username: 'sevilayha',
+  password: 'password' 
+});
+
+// call the custom method. this will just add -dude to his name
+// user will now be Chris-dude
+chris.dudify(function(err, name) {
+  if (err) throw err;
+
+  console.log('Your new name is ' + name);
+});
+
+// call the built-in save method to save to the database
+chris.save(function(err) {
+  if (err) throw err;
+
+  console.log('User saved successfully!');
+});
